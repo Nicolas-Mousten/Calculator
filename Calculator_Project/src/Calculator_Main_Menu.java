1673f07f78ba2f92e58dfbf49da81568472470dc
Nicolas-Mousten <nicolas.mousten@gmail.com>
2021-09-08 11:24:15 +0200

    Methods made

--- Calculator_Project/src/Calculator_Main_Menu.java
+++ Calculator_Project/src/Calculator_Main_Menu.java
@@ -1,7 +1,21 @@
 public class Calculator_Main_Menu {
+    public static double multi(int num1, int num2){
+        double sum = 0;
+        return sum;
+    }
+    public static double divison(int num1, int num2){
+        double sum = 0;
+        return sum;
+    }
+    public static double add(int num1, int num2){
+        double sum = 0;
+        return sum;
+    }
+    public static double sub(int num1, int num2) {
+        double sum = 0;
+        return sum;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello, there kanobi");
+
     }
 }
