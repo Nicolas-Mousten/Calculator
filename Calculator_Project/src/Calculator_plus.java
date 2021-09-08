876a2b04350300303a4aa68e3766ad7ecc0af1b8
Toby-byte <tobiasbogenielsen@gmail.com>
2021-09-08 11:23:14 +0200

    Hello,there commit

---
+++ Calculator_Project/src/Calculator_plus.java
@@ -0,0 +1,9 @@
+public class Calculator_plus {
+    public static int treintegers(int integers1, int integers2,int integers3) {
+        int sumOfIntegers = integers1 + integers2 + integers3;
+        return sumOfIntegers;
+    }
+    public static void main(String[] args) {
+
+    }
+}
