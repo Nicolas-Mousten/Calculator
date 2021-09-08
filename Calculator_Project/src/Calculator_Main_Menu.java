f03c9dca8246fb1576fc99976972a76bb988ef57
Nicolas-Mousten <nicolas.mousten@gmail.com>
2021-09-08 11:36:57 +0200

    Calculator logic Done!!!!!

--- Calculator_Project/src/Calculator_Main_Menu.java
+++ Calculator_Project/src/Calculator_Main_Menu.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Calculator_Main_Menu {
     public static double multi(int num1, int num2){
         double sum = 0;
@@ -20,6 +22,30 @@ public class Calculator_Main_Menu {
         return sum;
     }
     public static void main(String[] args) {
-
+        double sum;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose 2 whole numbers to calculate");
+        int num1 = scanner.nextInt();
+        scanner.nextLine();
+        int num2 = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Choose what operation to use by their icons");
+        String operations = scanner.nextLine();
+        if(operations=="*"){
+            sum = multi(num1,num2);
+            System.out.println(sum);
+        }
+        else if(operations=="/"){
+            sum = divison(num1,num2);
+            System.out.println(sum);
+        }
+        else if(operations=="+"){
+            sum = add(num1,num2);
+            System.out.println(sum);
+        }
+        else if(operations=="-"){
+            sum = sub(num1,num2);
+            System.out.println(sum);
+        }
     }
 }
